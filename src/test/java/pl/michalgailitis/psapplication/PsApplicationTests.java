449a136db85088c70b9e2c6457e053f5b6b23ddf
Michal <michal.gailitis@gmail.com>
2020-10-25 13:20:12 +0100

    Initialization of project

---
+++ src/test/java/pl/michalgailitis/psapplication/PsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michalgailitis.psapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
